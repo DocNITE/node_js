a519d5846864456f17602427bccb5ffb6e0593e5
DocNITE <docnite0530@gmail.com>
2023-07-25 22:16:50 +0300

    Some lol

--- client/src/index.ts
+++ client/src/index.ts
@@ -7,15 +7,6 @@ import { io } from "socket.io-client";
 let app = new PIXI.Application<HTMLCanvasElement>({ resizeTo: window });
 document.body.appendChild(app.view);
 
-/*
-const app = new PIXI.Application({
-  width: canvasWidth,
-  height: canvasHeight,
-  backgroundColor: 0x1099bb,
-  resolution: 2 * window.devicePixelRatio,
-});
- */
-
 let socket = io({reconnection: false});
 
 socket.on('sync', (data) => {
@@ -26,57 +17,10 @@ socket.on('sync', (data) => {
 // Initialize, when client was connected
 socket.on('init', (data) => {
     document.title = data.hostname;
-
-    
-    const dwallsAtlas: PIXI.ISpritesheetData = wallsAtlas;
-
-    //const spritesheet = new PIXI.Spritesheet(
-    //    PIXI.BaseTexture.from(wallsAtlas.meta.image),
-    //    dwallsAtlas
-    //);
-
-    const spritesheet: any = PIXI.Assets.load<PIXI.Spritesheet>('./../images/walls.json');
-    
-    // Generate all the Textures asynchronously
-    //async () => {
-    //    await spritesheet.parse(); 
-    //}
-
-    // spritesheet is ready to use!
-   //let aniInfo: any = spritesheet.animations["wall"];
-   // блять эта хуйня не может прочитать анимации. Че за нах нахуй? Сукааааа!
-    //const anim = new PIXI.AnimatedSprite(spritesheet.animations["wall"]);
-    console.log(spritesheet);
-    console.log(spritesheet.animations);
-    const anim = new PIXI.AnimatedSprite(spritesheet.animations.wall);
-    
-    // set the animation speed 
-    anim.animationSpeed = 0.1666;
-    
-    // play the animation on a loop
-    anim.play();
-    
-    // add it to the stage to render
-    app.stage.addChild(anim);
-    
-    app.ticker.add((delta) => {
-        //console.log(delta);
-    });
+    // TODO NEEd rework client. Maybe use another draw lib
 })
 
 socket.on('disconnect', (reason) => {
     console.log("Client network was closed!");
     socket.close();
 })
-
-//const socket = new WebSocket("ws://127.0.0.1:5500");
-//
-//// Connection opened
-//socket.addEventListener("open", (event) => {
-//    socket.send("Hello Server!");
-//});
-//  
-//// Listen for messages
-//socket.addEventListener("message", (event) => {
-//    console.log("Message from server ", event.data);
-//});
--- server/src/discord/bot.ts
+++ server/src/discord/bot.ts
@@ -1,10 +1,38 @@
-import {Client, GatewayIntentBits, Message} from 'discord.js'
+import {REST, Routes, Client, GatewayIntentBits, Message, SlashCommandBuilder} from 'discord.js'
 import config from './../settings/config.json'
+import fs from 'node:fs';
+import path from 'node:path';
 
 export class Bot {
     DiscordClient: Client;
+    RestAPI: REST;
 
     constructor() {
+        // Initialize commands
+        const command_test = 
+            {
+              data: new SlashCommandBuilder()
+                   .setName('ping')
+                   .setDescription('Replies with Pong!'),
+               async execute(interaction: any) {
+                  await interaction.reply('Pong!');
+              }
+            };
+        const commands = [];
+        commands.push(command_test.data.toJSON());
+        // Grab all the command files from the commands directory you created earlier
+        //const foldersPath = path.join(__dirname, 'commands');
+        //const commandFolders = fs.readdirSync(foldersPath);
+
+        // Construct and prepare an instance of the REST module
+        this.RestAPI = new REST().setToken(config.discord_bot_token);
+
+        // And deploy application commands!
+        this.RestAPI.put(
+            Routes.applicationCommands(config.discord_client_id),
+            { body: commands },
+        );
+
         // Initialize client
         this.DiscordClient = new Client({ 
             intents: [
@@ -13,9 +41,11 @@ export class Bot {
                 GatewayIntentBits.DirectMessages
             ] 
           });
+
         // Make events
         this.DiscordClient.on("ready", this.ready);
         this.DiscordClient.on("messageCreate", this.messageCreate)
+
         // Login and enable
         this.DiscordClient.login(config.discord_bot_token);
     }
